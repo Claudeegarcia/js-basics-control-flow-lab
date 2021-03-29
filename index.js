72f86ef8956c8fa872bd7e90cb72c58be58e7e64
Claudia <claudeegarcia@icloud.com>
2021-03-28 23:53:02 -0500

    commit tree da7b2d56ee9c7cfb98ec90003627d799020bbeac

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } 
+else if (2000 < feet && feet < 2501) {return 'I will gladly take your thirty bucks.'}
+
+else {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//use tenary function here. Tenary else/if statements are necessary. but 'return' is. ':' if not what the variable is set then or else the other side of ':' 
+function ternaryCheckCity(city) {
+  return city=='NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+if (tip === "generous") {
+  return 'Thank you so much.'
+}
+else if (tip == 'not as generous'){
+  return 'Thank you.'
+}
+else {
+  return 'Bye.'
+}
 }
